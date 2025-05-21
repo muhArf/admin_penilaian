12ae7f1f9b11ed4024bc8421ecc2176ec5a4a9b9
Muhamad Arif <143460423+muhArf@users.noreply.github.com>
2025-05-21 09:47:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/logo.svg" type="image/png">
+    <title>Adminstrasi Sekolah</title>
+</head>
+<body>
+    <!-- Konten lainnya -->
+    <script src="admin_penilaian/script.js"></script> <!-- Pastikan jalur ini benar -->
+</body>
+</html>
